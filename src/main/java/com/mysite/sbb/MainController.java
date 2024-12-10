88dd5e78cdf85bf477784a2be68b27a543946cb9
paek1127 <paek1127@gmail.com>
2024-12-10 10:52:23 +0900

    ResponseBody 추가

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,16 +2,25 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
     @GetMapping("/")
-    public void home() {
+    @ResponseBody
+    public int home() {
         System.out.println("Home page requested");
+        return 22;
     }
 
     @GetMapping("/about")
-    public void about() {
+    @ResponseBody
+    public String about() {
         System.out.println("about");
+        return "안녕하세요";
+    }
+
+    public void contact() {
+        System.out.println("contact");
     }
 }
