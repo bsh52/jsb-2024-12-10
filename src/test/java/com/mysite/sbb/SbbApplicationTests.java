0486e13f3cd905263e04216ccf1ac4b541667511
paek1127 <paek1127@gmail.com>
2024-12-16 23:39:31 +0900

    답변 데이터 저장

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,15 +1,17 @@
 package com.mysite.sbb;
 
 
+import com.mysite.sbb.domain.answer.entity.Answer;
+import com.mysite.sbb.domain.answer.repository.AnswerRepository;
 import com.mysite.sbb.domain.question.entity.Question;
 import com.mysite.sbb.domain.question.repository.QuestionRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
@@ -17,13 +19,19 @@ public class SbbApplicationTests {
     @Autowired
     private QuestionRepository questionRepository;
 
+    @Autowired
+    private AnswerRepository answerRepository;
+
     @Test
     void testJpa() {
-        assertEquals(2, questionRepository.count());
-        Optional<Question> oq = questionRepository.findById(1);
+        Optional<Question> oq = questionRepository.findById(2);
         assertTrue(oq.isPresent());
         Question q = oq.get();
-        questionRepository.delete(q);
-        assertEquals(1, questionRepository.count());
+
+        Answer a = new Answer();
+        a.setContent("네 자동으로 생성됩니다.");
+        a.setQuestion(q);
+        a.setCreateDate(LocalDateTime.now());
+        answerRepository.save(a);
     }
 }
