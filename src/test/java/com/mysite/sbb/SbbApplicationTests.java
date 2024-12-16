344ef0ea22f4ac670d4a0b703afa87c35345cb49
paek1127 <paek1127@gmail.com>
2024-12-16 23:20:39 +0900

    repository test

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,16 +1,31 @@
 package com.mysite.sbb;
 
 
+import com.mysite.sbb.domain.question.entity.Question;
+import com.mysite.sbb.domain.question.repository.QuestionRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 public class SbbApplicationTests {
+    @Autowired
+    private QuestionRepository questionRepository;
 
     @Test
-    @Transactional
     void testJpa() {
+        Question q1 = new Question();
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContent("sbb에 대해서 알고 싶습니다.");
+        q1.setCreateDate(LocalDateTime.now());
+        questionRepository.save(q1);
 
+        Question q2 = new Question();
+        q2.setSubject("스프링부트 모델 질문입니다.");
+        q2.setContent("id는 자동으로 생성되나요?");
+        q2.setCreateDate(LocalDateTime.now());
+        questionRepository.save(q2);
     }
 }
