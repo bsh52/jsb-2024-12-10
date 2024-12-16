19638fb48272b402ed9d4c6517d85ee1e1f1dff7
paek1127 <paek1127@gmail.com>
2024-12-16 23:51:27 +0900

    질문 목록 템플릿 추가

--- src/main/java/com/mysite/sbb/domain/question/controller/QuestionController.java
+++ src/main/java/com/mysite/sbb/domain/question/controller/QuestionController.java
@@ -1,15 +1,23 @@
 package com.mysite.sbb.domain.question.controller;
 
+import com.mysite.sbb.domain.question.entity.Question;
+import com.mysite.sbb.domain.question.repository.QuestionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class QuestionController {
+    private final QuestionRepository questionRepository;
 
     @GetMapping("/question/list")
-    @ResponseBody
-    public String list() {
-        return "question list";
+    public String list(Model model) {
+        List<Question> questionList = questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
+        return "question_list";
     }
 }
