6c2d4951e18960922f21e53759dbec562f3d9def
paek1127 <paek1127@gmail.com>
2024-12-16 23:37:06 +0900

    질문 데이터 삭제

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
@@ -18,10 +19,11 @@ public class SbbApplicationTests {
 
     @Test
     void testJpa() {
+        assertEquals(2, questionRepository.count());
         Optional<Question> oq = questionRepository.findById(1);
         assertTrue(oq.isPresent());
         Question q = oq.get();
-        q.setSubject("수정된 제목");
-        questionRepository.save(q);
+        questionRepository.delete(q);
+        assertEquals(1, questionRepository.count());
     }
 }
